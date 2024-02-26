5f5a5b91220bdcefe819e5f248add67db540c7a9
ds8010 <156882507+ds8010@users.noreply.github.com>
2024-02-26 10:46:21 +0400

    version1

---
+++ Grp2_Activity2.py
@@ -0,0 +1,5 @@
+"""
+Group 2
+Manifesto :This Program is written to act like a currency converter.
+           It has two options: Converting from AED to other currencies and Converting other currencies to AED.
+"""
