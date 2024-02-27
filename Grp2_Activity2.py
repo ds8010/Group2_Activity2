f04654d5c8145ee741c5812cefe1d0f2ed44a5ab
hamad-alsaadi <158991079+hamad-alsaadi@users.noreply.github.com>
2024-02-27 11:15:42 +0400

    Update Grp2_Activity2.py
    first part

--- Grp2_Activity2.py
+++ Grp2_Activity2.py
@@ -3,3 +3,58 @@ Group 2
 Manifesto :This Program is written to act like a currency converter.
            It has two options: Converting from AED to other currencies and Converting other currencies to AED.
 """
+'''constant conversion rates'''
+aed_euro=0.25          #conversion rate from AED to Euro
+aed_pound=0.22         #conversion rate from AED to Pound  
+aed_dol=0.27           #conversion rate from AED to Dollars 
+dol_aed=3.67           #conversion rate from Dollar to AED 
+pound_aed=4.64         #conversion rate from Pound to AED
+eur_aed=3.98           #conversion rate from Euro to AED 
+
+def aed_to_euro(money):
+"""converting from AED to EURO"""
+           eur=money*aed_eur
+           print(money,AED is equal to",eur,"EUR)
+def aed_to_britishpound(money):
+"""converting from AED to british pound""" 
+           pound=money*aed_pound
+           print(money,"AED is equal to",pound,"british pound")
+def aed_to_dollar(money):
+"""converting from aed to dollars"""
+           dollar=money*aed_dol
+           print(money,"AED is equal to",dollars,"USD")
+def dollars_to_aed(amount):
+"""converting from dollars to AED"""
+           aed=amount*dol_aed
+           print(amount,"USD is equal to",aed"AED")
+def pound_to_aed(amount):
+"""converting from Pound to AED"""
+           aed=amount*pound_aed
+           print(amount,"pound is equal to",aed,"AED)
+
+def eur_to_aed(amount):
+    """ converting from Euro to AED"""
+    aed=amount*eur_aed
+    print(amount,"EUR is equal to",aed,"AED")
+    
+
+                 
+
+
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
